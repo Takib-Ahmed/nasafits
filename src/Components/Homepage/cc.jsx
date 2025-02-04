fe915df4df3ec75106bfe0ab82606bdf1099ef81
Takib-Ahmed <satakib300@gmail.com>
2025-02-05 00:41:33 +0600

    text 5

--- src/Components/Homepage/cc.jsx
+++ src/Components/Homepage/cc.jsx
@@ -76,7 +76,7 @@ export default function All({
             ))}
 
           </div>
-                <div className=" flex justify-center w-full items-center">      <Link to='/shop' className=" flex w-fit justify-center  items-center"><Button size="md"  href='/shop'   variant="solid" color=""  className='bg-slate-700 text-white md:hidden' radius="md">Show More</Button><Button size="lg"    variant="solid" color=""  className=' hidden md:block bg-slate-700 text-white' radius="md">Show More</Button></Link></div></>
+                <div className=" flex justify-center w-full items-center">      <Link to='/shop' className="cursor-pointer" role="button"><Button    size="md"  href='/shop'   variant="solid" color=""  className='bg-slate-700 text-white md:hidden' radius="md">Show More</Button><Button size="lg"    variant="solid" color=""  className=' hidden md:block bg-slate-700 text-white' radius="md">Show More</Button></Link></div></>
         )}
       </div>
     </>
